3aeb04aa622078d43eb4326644dde5994add0588
Dhruv <dhruvgambhir077@gmail.com>
2026-02-15 23:53:47 +0530

    Add Pricing Page

---
+++ app/(main)/doctors/[speciality]/page.jsx
@@ -0,0 +1,14 @@
+"use client"
+import { useParams } from 'next/navigation'
+import React from 'react'
+
+const SpecialityPage = () => {
+    const {specialty} = useParams();
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SpecialityPage
